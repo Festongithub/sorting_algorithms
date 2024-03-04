36aa9a560a06bca99561fa7089be7669736d2cac
Festongithub <fkemboik@gmail.com>
2024-03-03 22:53:05 -0800

    sorting algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,19 +11,24 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, temp;
+	size_t i, j, temp = 0;
+
+	if (size < 2)
+	{
+		return;
+	}
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 1; j < size - i; j++)
+		for (j = 0; j < size; j++)
 		{
-			if (array[j - 1] > array[j])
+			if (array[j] > array[j + 1] && array[j + 1])
 			{
 				temp = array[j];
-				array[j] = array[j - 1];
-				array[j - 1] = temp;
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
 			}
-			print_array(array, size);
 		}
 	}
 }
