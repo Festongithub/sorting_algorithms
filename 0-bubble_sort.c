d46816d22cefefb79a5d1056326d760b4ce4112d
Festongithub <fkemboik@gmail.com>
2024-03-04 00:47:15 -0800

    sorting algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,34 +1,23 @@
 #include "sort.h"
-
 /**
- * bubble_sort - sorts an array of integers in ascending
- * order using the Bubble sort algorithm
- * @array: contains elements
- * @size: number of elements in array
- * Return: array
- *
- **/
-
+ * bubble_sort - sort array lements from min to max value
+ * @array: array
+ * @size: array size
+ */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, temp = 0;
 
-	if (size < 2)
-	{
-		return;
-	}
+	size_t i, index, tmp = 0;
 
 	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size; j++)
+		for (index = 0; index < size; index++)
 		{
-			if (array[j] > array[j + 1] && array[j + 1])
+			if (array[index] > array[index + 1] && array[index + 1])
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				print_array(array, size);
+			tmp = array[index];
+			array[index] = array[index + 1];
+			array[index + 1] = tmp;
+			print_array(array, size);
 			}
 		}
-	}
 }
