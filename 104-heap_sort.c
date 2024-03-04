3fe42244486f6405c914dd446785e1f97432da09
Festongithub <fkemboik@gmail.com>
2024-03-04 00:17:36 -0800

    sorting algorithms

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,56 +1,49 @@
 #include "sort.h"
-
-
-
-void sift_down(int *array, int i, int j,  size_t size)
-{
-	int largest = j;
-	int left = 2 * j + 1;
-	int right = 2 * j + 2;
-	int tmp;
-
-	if (left < i && array[left] > array[largest])
-	{
-		largest = left;
-	}
-
-	if (right < i  && array[right] > array[largest])
-	{
-                largest = right;
-        }
-
-	if (largest != j)
-	{
-		tmp = array[i];
-		array[j] = array[largest];
-		array[largest] = tmp;
-		print_array(array, size);
-		sift_down(array, i, largest, size);
-	}
-}
-
-
-
 /**
- * heap_sort - sorts an array of integers in
- * ascending order using the Heap sort algorithm
- * @array: container of elements
- * @size: length of array
- **/
-
+  * heap_sort - heap sort algorithm
+  * @array: array to sort
+  * @size: size of array
+  */
 void heap_sort(int *array, size_t size)
 {
 	int i, tmp;
 
 	for (i = size / 2 - 1; i >= 0; i--)
-		sift_down(array, i, size, size);
-
+		heapify(array, i, size, size);
 	for (i = size - 1; i >= 0; i--)
 	{
 		tmp = array[0];
 		array[0] = array[i];
 		array[i] = tmp;
 		print_array(array, size);
-		sift_down(array,i, 0, size);
+		heapify(array, i, 0, size);
+	}
+}
+
+/**
+  * heapify - heap sort algorithm
+  * @array: array to sort
+  * @idx: size of array
+  * @idx2: size of the second array
+  * @size: number of elements
+  */
+void heapify(int *array, int idx, int idx2, size_t size)
+{
+	int max = idx2;
+	int left = 2 * idx2 + 1;
+	int right = 2 * idx2 + 2;
+	int tmp;
+
+	if (left < idx && array[left] > array[max])
+		max = left;
+	if (right < idx && array[right] > array[max])
+		max = right;
+	if (max != idx2)
+	{
+		tmp = array[idx2];
+		array[idx2] = array[max];
+		array[max] = tmp;
+		print_array(array, size);
+		heapify(array, idx, max, size);
 	}
 }
